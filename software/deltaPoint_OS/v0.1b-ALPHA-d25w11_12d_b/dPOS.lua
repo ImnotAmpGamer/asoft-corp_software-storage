494dbae25deb8eb613244e8070d9ad695b66d8f5
ImnotAmpGamer <lgamonkar2010@gmail.com>
2025-03-18 20:57:26 +0530

    Uploaded system files (v0.1b-ALPHA-25w11_12d_b)

---
+++ software/deltaPoint_OS/v0.1b-ALPHA-d25w11_12d_b/dPOS.lua
@@ -0,0 +1,13 @@
+--[[
+deltaPoint OS (dPOS)
+Copyright © ASoft Corp 2025-2027
+All rights reserved.
+Do not distribute without permission!
+]]
+
+--OS Version Manager
+local ver = "v0.1b"
+local build_type = "ALPHA"
+local build_num = "25w11_12d_b"
+
+--OS Startup
